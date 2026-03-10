e3c4ee0eae2976ebe32afd9fcb403761e24a7de1
paula <paula.beatriz@upe.br>
2026-03-10 15:10:47 -0300

    defining props in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import './App.css'
+import { Person } from './Person';
+
+/*defining props
+let name: string = "Paula";
+let age: number = 200;
+let isMarried: boolean = false;
+
+let ages: number[] = [1,2,3,4,5];
+let person: any = 3;
+person = "whatever";
+*/
 
 function App() {
-
   return (
     <>
+      <Person name={"tony"} age={35} isMarried={true} />
+      <Person name={"maria"} age={22} isMarried={false} />
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/Person.tsx
@@ -0,0 +1,16 @@
+export interface Person {
+    name: string
+    age: number
+    isMarried: boolean
+}
+
+export const Person = (props: Person) => {
+    return (
+        <div>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>This person {props.isMarried ? "is married" : "is single(lonely)"}</p>
+        </div>
+    )
+    
+}
