aa58ce0c14552288270e906800d4a08aeac296f6
paula <paula.beatriz@upe.br>
2026-03-10 15:37:21 -0300

    hooks using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
 import './App.css'
 import { Person } from './Person';
 
-/*defining props
-let name: string = "Paula";
-let age: number = 200;
-let isMarried: boolean = false;
-
-let ages: number[] = [1,2,3,4,5];
-let person: any = 3;
-person = "whatever";
-*/
 
 function App() {
   return (
--- src/Person.tsx
+++ src/Person.tsx
@@ -1,3 +1,5 @@
+import { use, useState } from "react"
+
 export interface Person {
     name: string
     age: number
@@ -5,11 +7,24 @@ export interface Person {
 }
 
 export const Person = (props: Person) => {
+    const [personBio, setPersonBio] = useState<string | null>("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPersonBio(event.target.value);
+    }
+
     return (
         <div>
             <p>Name: {props.name}</p>
             <p>Age: {props.age}</p>
             <p>This person {props.isMarried ? "is married" : "is single(lonely)"}</p>
+
+            <p>
+                {props.name} 
+                Bio: {!personBio ? "No Bio Available" : personBio}
+            </p>
+
+            <input onChange={handleChange} />
         </div>
     )
     
